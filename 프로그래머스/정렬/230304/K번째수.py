c8b042c0b993e3b66f6d453a0510a249ef4df849
진민지 <67037321+realmingji@users.noreply.github.com>
2023-03-04 05:13:47 +0900

    Level 1 K번째수.py
    Level 1 K번째수.py

--- 프로그래머스/정렬/230304/K번째수.py
+++ 프로그래머스/정렬/230304/K번째수.py
@@ -1,1 +1,11 @@
-
+def solution(array, commands):
+    # array=[1,5,2,6,3,7,4], i=2, j=5, k=3
+    # i번째부터 j번째까지 자르기
+    # 배열 정렬 후 k번째 숫자 구하기
+    num = []
+    for i, j, k in commands:
+        # slice = array[i-1:j]
+        # slice.sort()
+        # count.append(slice[k-1])
+        num.append(sorted(array[i-1:j])[k-1])
+    return num
