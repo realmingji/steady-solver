febee4d2cedd6345b2ef35536df617419e9394d5
진민지 <67037321+realmingji@users.noreply.github.com>
2023-03-04 05:14:35 +0900

    Level1 K번째수.py
    Level1 K번째수.py

--- 프로그래머스/정렬/230304/K번째수.py
+++ 프로그래머스/정렬/230304/K번째수.py
@@ -1,7 +1,7 @@
 def solution(array, commands):
     # array=[1,5,2,6,3,7,4], i=2, j=5, k=3
     # i번째부터 j번째까지 자르기
-    # 배열 정렬 후 k번째 숫자 구하기
+    # 배열 정렬 후 k번째 숫자
     num = []
     for i, j, k in commands:
         # slice = array[i-1:j]
